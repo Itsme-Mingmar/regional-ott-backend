86ad9957723bd036fc46278379502564dfdc2ef4
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-03-06 17:26:42 +0545

    watchHistory): hodels for watch history

---
+++ src/models/WatchHistory.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const watchHistorySchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    video: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+      required: true,
+    },
+
+    progress: {
+      type: Number, // seconds watched
+      default: 0,
+    },
+
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+
+    watchedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+/*
+  Prevent duplicate history for same user + video
+*/
+watchHistorySchema.index({ user: 1, video: 1 }, { unique: true });
+
+const WatchHistory = mongoose.model("WatchHistory", watchHistorySchema);
+
+export default WatchHistory;
