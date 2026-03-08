debf5e59325f2c2484064769caf1d3e2d0403018
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-03-08 10:01:52 +0545

    feat(videoRoutes): added all routes of video controllers

---
+++ src/routes/videoRoutes.js
@@ -0,0 +1,38 @@
+import { Router } from "express";
+import { getAllMovies, getVideoById, uploadVideo, updateVideo, deleteVideo, getProvinceVideos } from "../controllers/video.controller.js";
+import { uploadVideoAndThumbnail } from "../middleware/multer.middleware.js";
+import verifyJWT from "../middleware/verifyJwt.middleware.js";
+import roleVerify from "../middleware/role.middleware.js";
+
+const videoRouter = Router();
+
+videoRouter.get("/movies", getAllMovies);
+videoRouter.get("/:videoId", getVideoById);
+videoRouter.get("/province/:provinceId/:category", getProvinceVideos);
+videoRouter.post("/upload",
+    verifyJWT,
+    roleVerify,
+    uploadVideoAndThumbnail.fields([
+        { name: 'video', maxCount: 1 },
+        { name: 'thumbnail', maxCount: 1 }
+    ]),
+    uploadVideo
+);
+
+videoRouter.put("/:videoId",
+    verifyJWT,
+    roleVerify,
+    uploadVideoAndThumbnail.fields([
+        { name: 'video', maxCount: 1 },
+        { name: 'thumbnail', maxCount: 1 }
+    ]),
+    updateVideo
+);
+
+videoRouter.delete("/:videoId",
+    verifyJWT,
+    roleVerify,
+    deleteVideo
+);
+
+export { videoRouter };
