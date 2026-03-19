227facc35250ca66ad0a10317f4e6e171ebc21dc
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-03-19 22:25:52 +0545

    route user controller

--- src/app.js
+++ src/app.js
@@ -8,10 +8,30 @@ import { watchRouter } from "./routes/watchHistoryRoutes.js";
 const app = express();
 
 app.use(express.json());
-// app.use(cors({
-//   origin: process.env.FRONTEND_URL,
-//   credentials: true
-// }));
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+
+    const allowedOrigins = [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+      "http://localhost:5176",
+      process.env.FRONTEND_URL
+    ].filter(Boolean);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error('Not allowed by CORS'));
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  optionsSuccessStatus: 200
+}));
 app.use(cookieParser());
 app.use("/api/user",userRouter);
 app.use("/api/video", videoRouter);
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,6 +5,7 @@ import {
   checkEmail,
   loginUser,
   getUserProfile,
+  logoutUser,
 } from "../controllers/user.controller.js";
 import verifyJWT from "../middleware/verifyJwt.middleware.js";
 
@@ -13,6 +14,7 @@ const userRouter = Router();
 userRouter.post("/registerUser", registerUser);
 userRouter.post("/checkEmail", checkEmail);
 userRouter.post("/login", loginUser);
+userRouter.post("/logout", verifyJWT, logoutUser);
 userRouter.put("/updatePlan", verifyJWT, updateUserPlan);
 userRouter.get("/profile", verifyJWT, getUserProfile);
 
